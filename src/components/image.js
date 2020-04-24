f6ba9cab52e26a821425d10cb078145a680b68de
Eloise Barrow <eloise@chartbeat.com>
2020-04-24 09:52:07 -0400

    wip query restructure - including all images

--- src/components/image.js
+++ src/components/image.js
@@ -13,24 +13,79 @@ import Img from "gatsby-image"
  * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-const Image = () => {
 
-  const data = useStaticQuery(graphql`
-    query {
-      calebImages: file(relativePath: { eq: "img-2.jpg" }) {
-        childImageSharp {
-          fluid(maxHeight: 490) {
-            ...GatsbyImageSharpFluid
-          }
-        }
+
+export const Image = () => {
+
+  // const data = useStaticQuery(graphql`
+  //   query {
+  //     calebImages: file(relativePath: { eq: "img-2.jpg" }) {
+  //       childImageSharp {
+  //         fluid(maxHeight: 490) {
+  //           ...GatsbyImageSharpFluid
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
+
+export const imageStyles = graphql`
+  fragment imageStyles on File {
+    childImageSharp {
+      fluid(maxHeight: 490) {
+        ...GatsbyImageSharpFluid
       }
     }
-  `)
+  }
+`
+
+export const query = graphql`
+  query {
+    image1: file(relativePath: { eq: "images/img-1.jpg" }) {
+      ...imageStyles
+    }
+    image2: file(relativePath: { eq: "images/img-2.jpg" }) {
+      ...imageStyles
+    }
+    image3: file(relativePath: { eq: "images/img-3.jpg" }) {
+      ...imageStyles
+    }
+    image4: file(relativePath: { eq: "images/img-4.jpg" }) {
+      ...imageStyles
+    }
+    image5: file(relativePath: { eq: "images/img-5.jpg" }) {
+      ...imageStyles
+    }
+    image6: file(relativePath: { eq: "images/img-6.jpg" }) {
+      ...imageStyles
+    }
+    image7: file(relativePath: { eq: "images/img-7.jpg" }) {
+      ...imageStyles
+    }
+    image8: file(relativePath: { eq: "images/img-8.jpg" }) {
+      ...imageStyles
+    }
+    image9: file(relativePath: { eq: "images/img-9.jpg" }) {
+      ...imageStyles
+    }
+    image10: file(relativePath: { eq: "images/img-10.jpg" }) {
+      ...imageStyles
+    }
+    image11: file(relativePath: { eq: "images/img-11.jpg" }) {
+      ...imageStyles
+    }
+    image12: file(relativePath: { eq: "images/img-12.jpg" }) {
+      ...imageStyles
+    }
+    image13: file(relativePath: { eq: "images/img-13.jpg" }) {
+      ...imageStyles
+    }
+  }
+`
 
   return <Img 
-            fluid={data.calebImages.childImageSharp.fluid}
+            // fluid={data.calebImages.childImageSharp.fluid}
+            fluid={query.image1}
             className="current-img"
             alt="Caleb Henderson portfolio images" />
 }
-
-export default Image
